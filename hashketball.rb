c365883156e1c6fe808a384da56052be72ef0238
darien bryant <darienbryant11@gmail.com>
2020-12-04 01:40:25 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,7 @@ def game_hash
       blocks:  1,
       slam_dunks: 1},
       
-      {player_name: "Reggi Evans",
+      {player_name: "Reggie Evans",
       number: 30 ,
       shoe: 14,
       points:  12 ,
